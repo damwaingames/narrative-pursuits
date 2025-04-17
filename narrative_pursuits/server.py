e14bcfe71c7ff4507fff732c531341ecaa68a634
AJ Griffiths <164574794+damwaingames@users.noreply.github.com>
2025-04-17 20:39:50 +0100

    Tweak includes, sample simple server just for testing

---
+++ narrative_pursuits/server.py
@@ -0,0 +1,30 @@
+import strawberry
+
+from typing import Union
+
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+
+app = FastAPI()
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
